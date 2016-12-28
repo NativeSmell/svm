d8d88d07f16926d6bf52900700c182a1806b237b
Zausalin Aleksey <zausalin2@gmail.com>
2016-12-28 20:57:28 +0300

    Add files via upload

---
+++ CMakeFiles/svm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/svm.dir/main.cpp.o"
+  "CMakeFiles/svm.dir/SVM.cpp.o"
+  "svm.pdb"
+  "svm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/svm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
